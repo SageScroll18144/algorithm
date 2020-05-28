86b364924159567171913bbeb1a7c6207203fc3d
Felipe Santos <lipe.fssantos063@gmail.com>
2020-05-27 21:30:31 -0300

    dfs melhorado

---
+++ bfs.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int maxn = 1e5+10;
+
+vector<int> graph[maxn];
+
+bool visited[maxn];
+
+void bfs(int s){
+    
+}
+
+int main(){
+
+    int n, m;//nós e aresta
+    cin>>n;
+    cin>>m;
+
+    for (int i = 1; i <= m; i++)
+    {
+        int u, v;
+        scanf("%d %d", &u, &v);
+
+        //bi-direcional
+        graph[u].push_back(v);
+        graph[v].push_back(u);
+
+    }
+    
+   
+
+    return 0;
+}
---
+++ dfs1.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int maxn = 1e5+10;
+
+vector<int> graph[maxn];
+
+bool visited[maxn];
+int comp[maxn];
+
+void dfs(int u, int cc){
+
+    visited[u] = true;
+    comp[u] = cc;
+
+    for(auto node : graph[u]){
+        if(!visited[node]){
+            dfs(node, cc);
+        }
+    }
+
+}
+
+int main(){
+
+    int n, m;//nós e aresta
+    cin>>n;
+    cin>>m;
+
+    for (int i = 1; i <= m; i++)
+    {
+        int u, v;
+        scanf("%d %d", &u, &v);
+
+        //bi-direcional
+        graph[u].push_back(v);
+        graph[v].push_back(u);
+
+    }
+    
+    int cc = 0;
+
+    for (int i = 1; i <= n; i++)
+    {
+        if(!visited[i]){
+            dfs(i, ++cc);
+        } 
+    }
+    printf("%d\n", (comp[1]==comp[3]));
+    cout<<cc;
+
+    return 0;
+}
