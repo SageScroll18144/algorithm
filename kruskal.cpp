135d4932198ddc3f8413ea5bfaa15eeab85bea97
Felipe Santos <lipe.fssantos063@gmail.com>
2020-06-20 20:05:52 -0300

    kruskal

---
+++ kruskal.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int maxn = 1e5+10;
+
+int ds[maxn];
+
+void dsBuild(){
+    for (int i = 0; i < maxn; i++) ds[i] = i;
+}
+
+int dsFind(int a){
+    if(ds[a]!=a) ds[a] = dsFind(ds[a]);
+    return ds[a];
+}
+
+void dsUnion(int a, int b){
+    a = dsFind(a);
+    b = dsFind(b);
+    if(a < b) swap(a, b);
+
+    ds[b] = a;
+}
+int kruskal(vector<pair<int, pair<int, int>>>& edges, int n){//edges -> < DISTANCIA de U para V>. Tamanho do Grafo(qtd de vertices)
+    dsBuild();
+    sort(edges.begin(), edges.end());
+
+    long long int totalWeight = 0;
+    
+    for(auto edge : edges){
+        int weight = edge.first;
+        int u = edge.second.first;
+        int v = edge.second.second;
+
+        if(dsFind(u)!=dsFind(v)){
+            dsUnion(u, v);
+            totalWeight+=weight;
+
+        }
+    }
+    return totalWeight;
+
+
+}
+
+int main(){
+
+    return 0;
+}
