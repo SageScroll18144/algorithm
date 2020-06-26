14087dc25930d90d6b858704f926fe3bc3172a8f
Felipe Santos <lipe.fssantos063@gmail.com>
2020-06-26 19:01:06 -0300

    minmax

---
+++ minmax.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int maxn = 1e5+10;
+const int max_depth = 5;
+const int inf = 1e9+7;
+
+vector<int> graph[maxn];
+int weight[maxn];
+
+int minmax(int node, int depth, bool isMaximazing){
+    if(depth==0 || depth==max_depth) return weight[node];
+    if(isMaximazing){
+        int value = -inf;
+        for(auto v : graph[node]){
+            value = max(value, minmax(v, --depth, false));
+        }
+        return value;
+    }else{
+        int value = inf;
+        for(auto v : graph[node]){
+            value = min(value, minmax(v, --depth, true));
+        }
+        return value;
+    }
+
+
+}
+
+int main(){
+
+    return 0;
+}
