b102f941ee0ddd924da79d3003275c9d3721e70b
Felipe Santos <lipe.fssantos063@gmail.com>
2020-07-11 11:52:30 -0300

    .

--- code/lcs.cpp
+++ code/lcs.cpp
@@ -18,7 +18,7 @@ int main(){
     for (int i = 1; i <= n; i++){
         for (int j = 1; i <= m; j++){
             if(a[i]==b[j]){
-                dp[i][j] = dp[i-1][j-1];
+                dp[i][j] = dp[i-1][j-1] + 1;
             }else{
                 dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
             }
