35fb427808b62a555ef43895c7b65f0ddb76927d
Felipe Santos <lipe.fssantos063@gmail.com>
2020-07-15 23:57:55 -0300

    .

--- code/contagem_inv.cpp
+++ code/contagem_inv.cpp
@@ -2,40 +2,41 @@
 
 using namespace std;
 
-const int inf = 1e9+7;
+int merge(vector<int> &v){
+    if(v.size()==1) return 0;
 
-void merge(vector<int> &v){
-    if(v.size()==1) return;
+    int inv = 0;
 
     vector<int> u1, u2;
 
     for(int i = 0; i < v.size()/2; i++) u1.push_back(v[i]);
     for(int i = v.size()/2; i < v.size(); i++) u2.push_back(v[i]);
+    
+    inv+=merge(u1);
+    inv+=merge(u2);
 
-    merge(u1);
-    merge(u2);
-
-    u1.push_back(inf);
-    u2.push_back(inf);
+    u1.push_back(INT_MAX);
+    u2.push_back(INT_MAX);
 
     int init1 = 0;
     int init2 = 0;
 
     for(int i = 0; i < v.size(); i++){
-        if(u1[init1] < u2[init2]){
+        if(u1[init1] <= u2[init2]){
             v[i] = u1[init1];
             init1++;
         }else{
             v[i] = u2[init2];
             init2++;
+
+            inv+=u1.size()-init1-1;
         }
+
     }
-    return;
+    return inv;
 }
 
 int main(){
-
-
 
     return 0;
 }
