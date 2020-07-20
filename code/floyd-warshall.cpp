95cd0ed9602d77733e648d97b7e97d039d5db85a
Felipe Santos <lipe.fssantos063@gmail.com>
2020-07-19 22:51:27 -0300

    .

--- code/floyd-warshall.cpp
+++ code/floyd-warshall.cpp
@@ -5,49 +5,37 @@ using namespace std;
 const int maxn = 110;
 const int inf = 1e9+7;
 
-int pesos[maxn][maxn];//Peso das arestas
 int dist[maxn][maxn];//Distância entre um par de vértices
 
 int n, m;
 
+//1-based
 void floyd_warshall(){
-    for (int i = 0; i < n; i++){
-        for (int j = 0; j < n; j++){
-            dist[i][j] = inf;
-        }
-        
-    }
-    
-    for (int i = 0; i < n; i++){
-        for (int j = 0; j < n; j++){
-            if(i == j) dist[i][j] = 0;
-            else dist[i][j] = pesos[i][j];
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n; j++){
+            if(i==j) dist[i][j] = 0;
+            else dist[i][j] = inf;
         }
     }
+    while (m--){
+        int u, v, w;
+        scanf("%d%d%d", &u, &v, &w);
 
-    for (int k = 0; k < n; k++){
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                dist[i][j] = min(dist[i][j], (dist[i][k] + dist[k][j]));
+        dist[i][j] = w;
+        dist[j][i] = w;
+    }
+    for(int k=1; k<=n; k++){
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                dist[i][j] = min(dist[i][j], dist[i][k]+dist[k][j]);
             }
         }
     }
-    
-    
-} 
 
+}
 
 int main(void){
-
-    memset(pesos, inf, sizeof pesos);
-    cin>>n>>m;
-    while (m--){
-        int u, v, p;
-        cin>>u>>v>>p;
-        pesos[u][v] = p;
-        pesos[v][u] = p; 
-    }
+    scanf("%d%d", &n, &m);
     floyd_warshall();
-
     return 0;
 }
